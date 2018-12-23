b0083661d2ff9e4be1e0e81aa0034bac9c0930ba
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-12-23 11:55:38 -0500

    add ignore

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
 class App extends Component {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const result = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9e5490f28b6766a50f5ada44f356c759&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await result.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
 }
+
 
 export default App;
